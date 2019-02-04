37727aeb9dfdf2d3c5d4df8e51fcf15c09dd750c
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-03 20:32:21 -0500

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinicjeremiah/SfgPetClinicJeremiahApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinicjeremiah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicJeremiahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicJeremiahApplication.class, args);
+    }
+
+}
+
