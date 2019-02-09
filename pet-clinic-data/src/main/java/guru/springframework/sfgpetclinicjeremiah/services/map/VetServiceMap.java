e4d1184ce7bf895c7b4720e813b50afdfd5f5e85
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-08 22:56:11 -0600

    impl map based services, closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicjeremiah/services/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinicjeremiah.services.map;
+
+
+import guru.springframework.sfgpetclinicjeremiah.model.Vet;
+import guru.springframework.sfgpetclinicjeremiah.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
