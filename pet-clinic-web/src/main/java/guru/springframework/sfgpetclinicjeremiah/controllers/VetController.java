83e70f5d23f78f99b402ca0218c4ea62e6177dca
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-10 10:16:13 -0600

    added vets list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicjeremiah/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicjeremiah/controllers/VetController.java
@@ -1,15 +1,23 @@
 package guru.springframework.sfgpetclinicjeremiah.controllers;
 
+import guru.springframework.sfgpetclinicjeremiah.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/", "", "index", "index.html"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
