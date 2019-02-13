ba8126c06b3d12948f92304e0f6591825aeb64bc
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-12 18:29:38 -0600

    added petTypeservice             closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicjeremiah/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinicjeremiah.services;
+
+import guru.springframework.sfgpetclinicjeremiah.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
