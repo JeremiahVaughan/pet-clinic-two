78d15426bd81b0cea4f84c41079e16ef4b1e6aff
Jeremiah Vaughan <jeremiah.vaughan1@student.ctuonline.edu>
2019-03-17 18:38:57 -0500

    more changes

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicjeremiah/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicjeremiah.services;
+
+import guru.springframework.sfgpetclinicjeremiah.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
